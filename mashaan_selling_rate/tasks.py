cbb5f060a922b4328190e74171ee1bf485000c1b
UsamaNaveed9 <usamanaveed9263@gmail.com>
2023-03-18 07:12:24 +0000

    fect:edit in hooks.py file

--- mashaan_selling_rate/tasks.py
+++ mashaan_selling_rate/tasks.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 import frappe
 
 @frappe.whitelist()
-def cal_selling_price():
+def cal_selling_price(method,self):
 	items = frappe.db.get_list('Item',
 			filters={
 				'disabled': 0
@@ -18,20 +18,21 @@ def cal_selling_price():
 		d_warehouse = frappe.db.get_value('Item Default', {'parent': i.name}, ['default_warehouse'])
 		val_rate = frappe.db.get_value('Bin', {'item_code': i.name, 'warehouse': d_warehouse}, ['valuation_rate'])
 
-		convt_margin = i.margin/100
-		cal_margin = convt_margin * val_rate
-		selling_rate = cal_margin + val_rate
+		if i.margin and val_rate:
+			convt_margin = i.margin/100
+			cal_margin = convt_margin * val_rate
+			selling_rate = cal_margin + val_rate
 
-		if frappe.db.exists("Item Price", {"item_code": i.item_code,"price_list":"Standard Selling","selling":1}):
-			name = frappe.get_value('Item Price',{'item_code': i.item_code,'price_list':'Standard Selling','selling':1},'name')
-			p_record = frappe.get_doc("Item Price",name)
-			p_record.price_list_rate = selling_rate
-			p_record.save()
-		else:
-			pr_add = frappe.new_doc("Item Price")
-			pr_add.item_code = i.item_code
-			pr_add.price_list = "Standard Selling"
-			pr_add.selling = 1
-			pr_add.price_list_rate = selling_rate
-			pr_add.save()
+			if frappe.db.exists("Item Price", {"item_code": i.name,"price_list":"Standard Selling","selling":1}):
+				name = frappe.get_value('Item Price',{'item_code': i.name,'price_list':'Standard Selling','selling':1},'name')
+				p_record = frappe.get_doc("Item Price",name)
+				p_record.price_list_rate = selling_rate
+				p_record.save()
+			else:
+				pr_add = frappe.new_doc("Item Price")
+				pr_add.item_code = i.name
+				pr_add.price_list = "Standard Selling"
+				pr_add.selling = 1
+				pr_add.price_list_rate = selling_rate
+				pr_add.save()
 		
